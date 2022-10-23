b38a2442c5a5d9075f0fbd7f0c66931cbb60e63e
danielvallejo1209 <116469513+danielvallejo1209@users.noreply.github.com>
2022-10-23 00:05:46 -0500

    Commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+//IMPORTACIONES
 const express = require("express");
 const morgan = require ("morgan");
+const cors = require ("cors");
+const mongoose = require ("mongoose");
+//CONFIGURACIONES
 const app = express();
 const env = process.env;
 const port = env.PORT || 3000;
 app.use(morgan("dev"));
+app.use(cors());
+
+//ARRANQUE
 app.listen(port, ()=>{
     console.log("API iniciado en el puerto " + port);
 });
 
-
+//RUTAS
 app.get("/", (request, response) => {
     response.send("Hola Mundo");
 });
